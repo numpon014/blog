f3a5443ec44a270f88141274409c8d6237edd6dc
Numpon Maytangsuphut <numpon014@gmail.com>
2023-11-19 22:10:30 +0700

    renderers image with the next image component

--- src/components/posts/post-detail/post-content.tsx
+++ src/components/posts/post-detail/post-content.tsx
@@ -2,6 +2,7 @@ import styles from "./post-content.module.css";
 import PostHeader from "@/components/posts/post-detail/post-header";
 import IPost from "@/model/post";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 type Props = {
   post: IPost;
@@ -14,10 +15,28 @@ const PostContent = (props: Props) => {
 
   const imagePath = `/images/posts/${slug}/${image}`;
 
+  const customRenderers = {
+    image(image: any) {
+      return (
+        <Image
+          src={`/images/posts/${slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
   return (
     <article className={styles.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          img: customRenderers.image,
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </article>
   );
 };

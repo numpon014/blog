53251ac6f69dd79e802ccf36c3996eccc621ca83
Numpon Maytangsuphut <numpon014@gmail.com>
2023-11-19 20:52:59 +0700

    added markdown

--- src/components/posts/post-detail/post-content.tsx
+++ src/components/posts/post-detail/post-content.tsx
@@ -1,6 +1,7 @@
 import styles from "./post-content.module.css";
 import PostHeader from "@/components/posts/post-detail/post-header";
 import IPost from "@/model/post";
+import ReactMarkdown from "react-markdown";
 
 type Props = {
   post: IPost;
@@ -12,12 +13,11 @@ const PostContent = (props: Props) => {
   } = props;
 
   const imagePath = `/images/posts/${slug}/${image}`;
+
   return (
     <article className={styles.content}>
       <PostHeader title={title} image={imagePath} />
-      <div>
-        <p>{content}</p>
-      </div>
+      <ReactMarkdown>{content}</ReactMarkdown>
     </article>
   );
 };
